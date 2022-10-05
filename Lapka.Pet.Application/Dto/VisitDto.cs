ce93033aaf792fea0a7d2935ab4ea38d2d6e1d2f
Adrian Nieckarz <adrian.nieckarz@outlook.com>
2022-10-05 12:43:17 +0200

    Refactored Visit response

--- Lapka.Pet.Application/Dto/VisitDto.cs
+++ Lapka.Pet.Application/Dto/VisitDto.cs
@@ -2,6 +2,7 @@
 
 public class VisitDto
 {
+    public bool? HasTookPlace { get; set; }
     public Guid VisitId { get; set; }
     public DateTime DateOfVisit { get; set; }
     public string Description { get; set; }
--- Lapka.Pet.Infrastructure/Mapper/VisitMapper.cs
+++ Lapka.Pet.Infrastructure/Mapper/VisitMapper.cs
@@ -5,9 +5,19 @@ namespace Lapka.Pet.Infrastructure.Mapper;
 
 internal static class VisitMapper
 {
-    public static VisitDto AsVisitDto(this Visit visit)
+    public static VisitDto AsLastVisitDto(this Visit visit)
         => new()
         {
+            HasTookPlace = visit.HasTookPlace,
+            VisitId = visit.VisitId,
+            Description = visit.Description,
+            DateOfVisit = visit.DateOfVisit,
+        };
+
+    public static VisitDto AsIncomingVisitDto(this Visit visit)
+        => new()
+        {
+            HasTookPlace = null,
             VisitId = visit.VisitId,
             Description = visit.Description,
             DateOfVisit = visit.DateOfVisit,
